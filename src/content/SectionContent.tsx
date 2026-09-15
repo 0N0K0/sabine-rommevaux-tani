914a84dc61a0f2b7998ec1b51b47cbfb944687ae
NKoelblen <hello@onoko.dev>
2026-09-15 19:00:44 +0200

    recursive Sections

--- src/content/SectionContent.tsx
+++ src/content/SectionContent.tsx
@@ -1,15 +1,29 @@
+import { Title, type TitleOrder } from '@mantine/core';
 import type { Content, Data } from '../types/data';
 import { isData } from '../types/guards';
 import { ItemContent } from './ItemContent';
 
-export function SectionContent({ data }: { data: Data }) {
+interface SectionContentProps {
+  data: Data;
+  level?: TitleOrder;
+}
+
+export function SectionContent({ data, level = 2 }: SectionContentProps) {
   return (
     <section>
-      {data.title && <h2>{data.title}</h2>}
+      {data.title && <Title order={level}>{data.title}</Title>}
+
       {data.content?.map((item, index) => {
         if (isData(item)) {
-          return <SectionContent key={index} data={item as Data} />;
+          return (
+            <SectionContent
+              key={index}
+              data={item as Data}
+              level={Math.min(level + 1, 6) as TitleOrder}
+            />
+          );
         }
+
         return <ItemContent key={index} content={item as Content} />;
       })}
     </section>
