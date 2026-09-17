29110ca8ebdc531acf396ff5c12ebdde3d3c84db
NKoelblen <hello@onoko.dev>
2026-09-17 10:10:57 +0200

    theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { MantineProvider } from '@mantine/core'
-import '@mantine/core/styles.css'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import './style.css';
+import App from './App.tsx';
+import { theme } from './theme/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider classNamesPrefix="onoko">
+    <MantineProvider
+      classNamesPrefix="onoko"
+      forceColorScheme="light"
+      theme={theme}
+    >
       <App />
     </MantineProvider>
   </StrictMode>,
-)
+);
---
+++ src/theme/theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from '@mantine/core';
+
+export const theme = createTheme({
+  primaryColor: 'gold',
+
+  colors: {
+    gold: [
+      '#F8F5EF',
+      '#EDE5D8',
+      '#DFD1BD',
+      '#D0BFA2',
+      '#C1AD8D',
+      '#AF926C',
+      '#9A7D58',
+      '#836947',
+      '#6D563A',
+      '#57432E',
+    ],
+
+    red: [
+      '#FCEEEE',
+      '#F8DCDC',
+      '#F2B8B8',
+      '#E88E8E',
+      '#D96666',
+      '#C74646',
+      '#B53636',
+      '#9B2C2C',
+      '#812323',
+      '#671B1B',
+    ],
+
+    royalBlue: [
+      '#EEF2FC',
+      '#DCE4F7',
+      '#B9C7EC',
+      '#91A7DE',
+      '#6B87D0',
+      '#4A68BA',
+      '#3858A8',
+      '#2F4FA3',
+      '#263F85',
+      '#1E326A',
+    ],
+  },
+});
