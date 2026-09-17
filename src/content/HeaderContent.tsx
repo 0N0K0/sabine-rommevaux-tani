7a9ecccb8cae2a0e233466dd9192ffa2073ed91a
NKoelblen <hello@onoko.dev>
2026-09-17 09:37:54 +0200

    Header Content

--- src/content/HeaderContent.tsx
+++ src/content/HeaderContent.tsx
@@ -1,10 +1,64 @@
-import { Text, Title } from '@mantine/core';
+import { Anchor, Text, Title } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import type { Data, Detail } from '../types/data';
 
 export function HeaderContent() {
+  const [data, setData] = useState<Data>({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function loadHeader() {
+      try {
+        const response = await fetch('/data/header.json', {
+          cache: 'no-store',
+        });
+
+        if (!response.ok) {
+          throw new Error('Impossible de charger header.json');
+        }
+
+        const content: Data = await response.json();
+
+        setData(content);
+      } catch (error) {
+        setError(
+          error instanceof Error ? error : new Error('Une erreur est survenue'),
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadHeader();
+  }, []);
+
+  if (loading && !data) return <p>Chargement...</p>;
+
+  if (error) return <p>{error.message}</p>;
+
   return (
     <>
-      <Title order={1}>Sabine Rommevaux-Tani</Title>
-      <Text>Directrice de recherche, CNRS, AUSONIUS</Text>
+      <Title order={1}>{data.title}</Title>
+      <Text>
+        {data.content?.map((detail: Detail, index) => (
+          <>
+            {detail.link ? (
+              <Anchor
+                href={detail.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {detail.value}
+              </Anchor>
+            ) : (
+              <span>{detail.value}</span>
+            )}
+
+            {index < data.content!.length - 1 && ', '}
+          </>
+        ))}
+      </Text>
     </>
   );
 }
