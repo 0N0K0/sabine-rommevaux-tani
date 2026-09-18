d3c7c3193ab7aaf3c21be0e6e127e82c2dbb8a30
NKoelblen <hello@onoko.dev>
2026-09-18 10:54:34 +0200

    pdf preview

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import MentionsLegales from './pages/MentionsLegales.tsx';
 import { theme } from './theme/theme.ts';
+import { PdfPreview } from './pages/PdfPreview.tsx';
 
 const path = window.location.pathname;
 
-const Page = path === '/mentions-legales' ? MentionsLegales : App;
+const Page =
+  path === '/mentions-legales'
+    ? MentionsLegales
+    : path === '/pdf-preview'
+      ? PdfPreview
+      : App;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
