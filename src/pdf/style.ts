51a0c04d270fc23ea0a46f9b2bee086c36646ee6
NKoelblen <hello@onoko.dev>
2026-09-18 12:46:32 +0200

    style

--- src/pdf/style.ts
+++ src/pdf/style.ts
@@ -1,95 +1,109 @@
 import { Font, StyleSheet } from '@react-pdf/renderer';
+import MontserratThin from '../assets/fonts/Montserrat-Thin.ttf';
+import MontserratThinItalic from '../assets/fonts/Montserrat-ThinItalic.ttf';
+import MontserratExtraLight from '../assets/fonts/Montserrat-ExtraLight.ttf';
+import MontserratExtraLightItalic from '../assets/fonts/Montserrat-ExtraLightItalic.ttf';
+import MontserratLight from '../assets/fonts/Montserrat-Light.ttf';
+import MontserratLightItalic from '../assets/fonts/Montserrat-LightItalic.ttf';
+import MontserratRegular from '../assets/fonts/Montserrat-Regular.ttf';
+import MontserratRegularItalic from '../assets/fonts/Montserrat-Italic.ttf';
+import MontserratMedium from '../assets/fonts/Montserrat-Medium.ttf';
+import MontserratMediumItalic from '../assets/fonts/Montserrat-MediumItalic.ttf';
+import MontserratSemiBold from '../assets/fonts/Montserrat-SemiBold.ttf';
+import MontserratSemiBoldItalic from '../assets/fonts/Montserrat-SemiBoldItalic.ttf';
+import MontserratBold from '../assets/fonts/Montserrat-Bold.ttf';
+import MontserratBoldItalic from '../assets/fonts/Montserrat-BoldItalic.ttf';
+import MontserratExtraBold from '../assets/fonts/Montserrat-ExtraBold.ttf';
+import MontserratExtraBoldItalic from '../assets/fonts/Montserrat-ExtraBoldItalic.ttf';
+import MontserratBlack from '../assets/fonts/Montserrat-Black.ttf';
+import MontserratBlackItalic from '../assets/fonts/Montserrat-BlackItalic.ttf';
+import CormorantUnicaseLight from '../assets/fonts/CormorantUnicase-Light.ttf';
+import CormorantUnicaseRegular from '../assets/fonts/CormorantUnicase-Regular.ttf';
+import CormorantUnicaseMedium from '../assets/fonts/CormorantUnicase-Medium.ttf';
+import CormorantUnicaseSemiBold from '../assets/fonts/CormorantUnicase-SemiBold.ttf';
+import CormorantUnicaseBold from '../assets/fonts/CormorantUnicase-Bold.ttf';
 
 Font.register({
   family: 'Montserrat',
   fonts: [
     {
-      src: '/src/assets/fonts/Montserrat-Thin.ttf',
+      src: MontserratThin,
       fontWeight: 100,
     },
     {
-      src: '/src/assets/fonts/Montserrat-ThinItalic.ttf',
+      src: MontserratThinItalic,
       fontWeight: 100,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-ExtraLight.ttf',
+      src: MontserratExtraLight,
       fontWeight: 200,
     },
     {
-      src: '/src/assets/fonts/Montserrat-ExtraLightItalic.ttf',
+      src: MontserratExtraLightItalic,
       fontWeight: 200,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-Light.ttf',
+      src: MontserratLight,
       fontWeight: 300,
     },
     {
-      src: '/src/assets/fonts/Montserrat-LightItalic.ttf',
+      src: MontserratLightItalic,
       fontWeight: 300,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-Regular.ttf',
+      src: MontserratRegular,
       fontWeight: 400,
     },
     {
-      src: '/src/assets/fonts/Montserrat-RegularItalic.ttf',
+      src: MontserratRegularItalic,
       fontWeight: 400,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-Medium.ttf',
+      src: MontserratMedium,
       fontWeight: 500,
     },
     {
-      src: '/src/assets/fonts/Montserrat-MediumItalic.ttf',
+      src: MontserratMediumItalic,
       fontWeight: 500,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-SemiBold.ttf',
+      src: MontserratSemiBold,
       fontWeight: 600,
     },
     {
-      src: '/src/assets/fonts/Montserrat-SemiBoldItalic.ttf',
+      src: MontserratSemiBoldItalic,
       fontWeight: 600,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-Bold.ttf',
+      src: MontserratBold,
       fontWeight: 700,
     },
     {
-      src: '/src/assets/fonts/Montserrat-BoldItalic.ttf',
+      src: MontserratBoldItalic,
       fontWeight: 700,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-ExtraBold.ttf',
+      src: MontserratExtraBold,
       fontWeight: 800,
     },
     {
-      src: '/src/assets/fonts/Montserrat-ExtraBoldItalic.ttf',
-      fontWeight: 800,
-      fontStyle: 'italic',
-    },
-    {
-      src: '/src/assets/fonts/Montserrat-ExtraBold.ttf',
-      fontWeight: 800,
-    },
-    {
-      src: '/src/assets/fonts/Montserrat-ExtraBoldItalic.ttf',
+      src: MontserratExtraBoldItalic,
       fontWeight: 800,
       fontStyle: 'italic',
     },
     {
-      src: '/src/assets/fonts/Montserrat-Black.ttf',
+      src: MontserratBlack,
       fontWeight: 900,
     },
     {
-      src: '/src/assets/fonts/Montserrat-BlackItalic.ttf',
+      src: MontserratBlackItalic,
       fontWeight: 900,
       fontStyle: 'italic',
     },
@@ -100,45 +114,105 @@ Font.register({
   family: 'Cormorant Unicase',
   fonts: [
     {
-      src: '/src/assets/fonts/CormorantUnicase-Light.ttf',
+      src: CormorantUnicaseLight,
       fontWeight: 300,
     },
     {
-      src: '/src/assets/fonts/CormorantUnicase-Regular.ttf',
+      src: CormorantUnicaseRegular,
       fontWeight: 400,
     },
     {
-      src: '/src/assets/fonts/CormorantUnicase-Medium.ttf',
+      src: CormorantUnicaseMedium,
       fontWeight: 500,
     },
     {
-      src: '/src/assets/fonts/CormorantUnicase-SemiBold.ttf',
+      src: CormorantUnicaseSemiBold,
       fontWeight: 600,
     },
     {
-      src: '/src/assets/fonts/CormorantUnicase-Bold.ttf',
+      src: CormorantUnicaseBold,
       fontWeight: 700,
     },
   ],
 });
 
+export type HeadingLevel = 1 | 2 | 3 | 4;
+
 export const styles = StyleSheet.create({
   page: {
-    marginTop: '2cm',
-    marginBottom: '2cm',
-    marginLeft: '2.5cm',
-    marginRight: '2.5cm',
+    paddingTop: '2cm',
+    paddingBottom: '2cm',
+    paddingLeft: '2.5cm',
+    paddingRight: '2.5cm',
     fontFamily: 'Montserrat',
     fontSize: 12,
   },
+
   header: {
     position: 'absolute',
-    top: '-1cm',
-    height: '1cm',
+    top: '1cm',
+    left: '2.5cm',
+    right: '2.5cm',
   },
-  title: {
+
+  headerText: {
+    textAlign: 'right',
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: '1cm',
+    left: '2.5cm',
+    right: '2.5cm',
+    textAlign: 'right',
+  },
+
+  h1: {
     fontFamily: 'Cormorant Unicase',
-    fontWeight: 600,
+    fontSize: 42,
+  },
+
+  h2: {
+    fontFamily: 'Cormorant Unicase',
+    fontSize: 36,
+  },
+
+  h3: {
+    fontFamily: 'Cormorant Unicase',
+    fontSize: 30,
+  },
+
+  h4: {
+    fontFamily: 'Cormorant Unicase',
     fontSize: 24,
+  },
+
+  h5: {
+    fontFamily: 'Cormorant Unicase',
+    fontSize: 18,
+  },
+
+  h6: {
+    fontFamily: 'Cormorant Unicase',
+    fontSize: 12,
+  },
+
+  list: {
+    flexDirection: 'column',
+    width: '100%',
+  },
+
+  listItem: {
+    flexDirection: 'row',
+    gap: 6,
+  },
+
+  number: {
+    width: 24,
+    textAlign: 'right',
+  },
+
+  content: {
+    flex: 1,
   },
 });
